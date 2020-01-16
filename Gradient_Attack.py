bc245309259e2615586e2de826e1160bbfc1690c
lidora <lidora@playtika.com>
2020-01-16 19:15:29 +0200

    added gradient attack from notebook enable to run expriment

--- Gradient_Attack.py
+++ Gradient_Attack.py
@@ -86,7 +86,7 @@ def get_model(n_fake_users, n_users, n_movies):
                   layers=layers,
                   reg_layers=reg_layers,
                   reg_mf=reg_mf)
-    print(f'model with: n_fake_users={n_fake_users}, n_users={n_users}, n_movies={n_movies} created')
+    # print(f'model with: n_fake_users={n_fake_users}, n_users={n_users}, n_movies={n_movies} created')
     return model
 
 def get_batches(training_set, batch_size):
@@ -96,9 +96,8 @@ def get_batches(training_set, batch_size):
     batches = np.array_split(training_mat, training_mat.shape[0] // batch_size)
     return batches
 
-def train_model(N_FAKE_USERS, n_users, n_movies, train_set, test_set):
+def train_model(N_FAKE_USERS, n_users, n_movies, train_set, test_set, model_path):
     import os
-    model_path = f"tf_neumf_models/model{N_FAKE_USERS}.ckpt"
     if os.path.exists(model_path + '.index'):
         print("Model already exists at:", model_path)
         return
@@ -131,39 +130,61 @@ def train_model(N_FAKE_USERS, n_users, n_movies, train_set, test_set):
                                                                                      epoch_loss))
         save_path = saver.save(sess, model_path)
 
-def predict(n_fake_users, n_users, n_movies, test_set):
+def predict(n_fake_users, n_users, n_movies, test_set, model_path):
     tf.reset_default_graph()
     with tf.Session() as sess:
         model = get_model(n_fake_users=n_fake_users, n_users=n_users, n_movies=n_movies)
         saver = tf.train.Saver()
-        saver.restore(sess, "model.ckpt")
+        saver.restore(sess, model_path)
         model_p = TFPredictWrapper(model, sess)
         mean_hr, mean_ndcg, time_eval = evaluate_model(model_p, test_set, verbose=0)
         print('hr={:.3f} ndcg={:.3f} time={:.3f}'.format(mean_hr, mean_ndcg, time_eval))
 
-def create_attack_input(n_fake_users, n_users, n_movies, train_set):
+
+def create_attack_input(n_fake_users, n_users, n_movies, train_set, attack_rating_init_prob=0.5):
     """
-    This create a tuple of cartesian product of fake_users * n_movies
-    Which later will be fed to the algorithm to generate fake ratings probabilities
+    THERE ARE 1682 movies
+    Creates the attack input that will be used to generate best fake ratings combination.
+    It creates every combination of fake_user * item option and a initial rating prob.
+    Then, it concatenetes the real training set in the end of each list of user, item, rating
     :param n_fake_users:
     :param n_users:
     :param n_movies:
     :param train_set:
-    :return:
+    :param attack_rating_init_prob:
+    :return: A tuple of (user, item, rating), list of attack users, list of attack_items, output_dim used as indexer, every entry smaller is related to fake user input.
     """
-    output_dim = (n_fake_users - 1) * (n_movies - 1)
+    # create an array with fake_users*items (note for +1 for inclusive of np.arange)
     cartesian_prodcut = np.array([[n_users + user - 1, item]
-                                  for user in np.arange(1, n_fake_users) for item in np.arange(1, n_movies)])
+                                  for user in np.arange(1, n_fake_users + 1) for item in np.arange(0, n_movies)])
     attack_users = cartesian_prodcut[:, 0]
     attack_items = cartesian_prodcut[:, 1]
-    attack_user_item_cartesian = np.array([np.append(attack_users, train_set[0]),
-                                 np.append(attack_items, train_set[1])])
+    attack_output_dim = n_fake_users * n_movies
+    attack_ratings_prob = np.full((attack_output_dim,), attack_rating_init_prob)
+    attack_input = np.array([np.append(attack_users, train_set[0]),
+                             np.append(attack_items, train_set[1]),
+                             np.append(attack_ratings_prob, train_set[2])])
     real_training_rating_size = len(train_set[2])
-    print('attack rating size=', output_dim)
+    print('attack rating size=', attack_output_dim)
     print('real_training_rating_size=', real_training_rating_size)
-    return attack_user_item_cartesian, attack_users, attack_items
+    # assert np.max(attack_items) == train_set[1].max() # TODO THIS FAILS
+    assert np.max(attack_users) == n_users + n_fake_users - 1
+    assert len(np.unique(attack_users)) == n_fake_users
+    assert len(attack_input[0]) == len(attack_input[1])
+    assert len(attack_input[0]) == len(attack_input[2])
+
+    # every index that is SMALL from output_dim is related to a fake user input
+    return attack_input, attack_users, attack_items, attack_output_dim
 
 def filter_ratings_by_treshold(attack_users, attack_items, attack_rating_prob, threshold):
+    """
+
+    :param attack_users:
+    :param attack_items:
+    :param attack_rating_prob:
+    :param threshold:
+    :return: attack_df_filterd, len(attack_df_filterd)
+    """
     # for each user, we want to keep all positive ones, and keep excatly positive_ones* 4 zeros.
     import pandas as pd
 
@@ -173,15 +194,15 @@ def filter_ratings_by_treshold(attack_users, attack_items, attack_rating_prob, t
     attack_df_filterd = base_attack_df[base_attack_df.apply(lambda x: x.r_prob > threshold, axis=1)]  # apply on rows, keep only high prob ratings
     attack_df_filterd['r'] = attack_df_filterd['r_prob'].apply(lambda x: 1)  # |.astype(int)
     print('Amount of total poison after filter:', len(attack_df_filterd))
-    return attack_df_filterd
+    return attack_df_filterd, len(attack_df_filterd)
 
 def mix_attack_df_with_training_set(mal_training_set, train_set, TRAIN_FRAC=0.1):
     from Data import create_subset, concat_and_shuffle
-    print('train_set', len(train_set[0]))
     train_set_subset = create_subset(train_set, train_frac=TRAIN_FRAC)
     attack_benign_training_set = concat_and_shuffle(mal_training_set, train_set_subset)
-    print(len(mal_training_set[0]))
-    len(attack_benign_training_set[0])
+    # print('train_set', len(train_set[0]))
+    # print(len(mal_training_set[0]))
+    # len(attack_benign_training_set[0])
     return attack_benign_training_set
 
 def add_negative_samples(attack_df_filterd, num_negatives):
@@ -216,16 +237,14 @@ def add_negative_samples(attack_df_filterd, num_negatives):
     mal_training_set = (np.array(user_input), np.array(item_input), np.array(labels))
     return mal_training_set  # this already has the inputs and negative items
 
-def generate_fake_ratings(n_fake_users, n_users, n_movies, train_set, attack_train_set):
+def generate_fake_ratings(n_fake_users, n_users, n_movies, attack_input, model_path):
     """
     performs gradient step in order to get which ratings should be changed to increase the loss
     :return:
     """
-    model_path = f"tf_neumf_models/model{n_fake_users}.ckpt"
-    output_dim = (n_fake_users - 1) * (n_movies - 1)
-    real_training_rating_size = len(train_set[0])
+    output_dim = n_fake_users * n_movies
     tf.reset_default_graph()
-    mal_real_shape = output_dim + real_training_rating_size
+    mal_real_shape = len(attack_input[0])
     rating_input = tf.placeholder(tf.float32, [mal_real_shape], name='ratings_prob')
     model = get_model(n_fake_users=n_fake_users, n_users=n_users, n_movies=n_movies)
     print('rating_input.shape:', rating_input.shape)
@@ -244,12 +263,11 @@ def generate_fake_ratings(n_fake_users, n_users, n_movies, train_set, attack_tra
         eps = np.full((mal_real_shape,), step)
         # try to emulate a session with all data - training_set + adver, then the gradient will adjust adver rating params
         #     new_labels = np.full((output_dim,), 0.01) # initiate new ratings at 0.5
-        new_labels_mal_part = np.full((output_dim,), 0.5)
-        new_labels = np.concatenate([new_labels_mal_part, train_set[2]])
-        all_users = attack_train_set[0]
-        all_items = attack_train_set[1]
+
+        all_users = attack_input[0]
+        all_items = attack_input[1]
+        all_rating = attack_input[2]
         indexes = np.arange(len(all_users))
-        # new_labels.shape
         for i in range(20):
             ## todo: There is an issue in creating a shuffle while keeping track in the order of the mal data
 
@@ -259,34 +277,36 @@ def generate_fake_ratings(n_fake_users, n_users, n_movies, train_set, attack_tra
             #         all_items = all_items[p]
             #         new_labels = new_labels[p]
             #         new_labels_before_change = new_labels.copy()
-            mask = np.zeros((mal_real_shape,))
             #         indexes = indexes[p] # this will help get the mal ratings in order as we started
+
+            # mask the gradient such that benign user rating will not be changed
+            mask = np.zeros((mal_real_shape,))
             mask[np.argwhere(indexes < output_dim)] = 1
 
-            new_labels, adv_l = sess.run([rating_input_out, new_loss],
+            all_rating, adv_l = sess.run([rating_input_out, new_loss],
                                          feed_dict={model.user_inp: all_users,
                                                     model.item_inp: all_items,
-                                                    rating_input: new_labels,
+                                                    rating_input: all_rating,
                                                     mask_p: mask,
                                                     eps_p: eps})
-            new_labels = new_labels.reshape((-1,))
+            all_rating = all_rating.reshape((-1,))
             #         new_labels[np.argwhere(indexes >= output_dim)] = new_labels_before_change[np.argwhere(indexes >= output_dim)]
-            attack_rating_prob = new_labels[np.argwhere(indexes < output_dim)][np.arange(output_dim)].reshape(-1, )
+            attack_rating_prob = all_rating[np.argwhere(indexes < output_dim)][np.arange(output_dim)].reshape(-1, )
             ## Safety Check such that legit ratings did not change
-            legit_rating = new_labels[np.argwhere(indexes > output_dim)].reshape(-1, )
+            legit_rating = all_rating[np.argwhere(indexes > output_dim)].reshape(-1, )
             assert len(np.unique(legit_rating)) == 2, 'legit rating must not change during gradient step'
             print(i, np.round(attack_rating_prob[:10], 2), np.mean(attack_rating_prob))
         return attack_rating_prob
 
 
-def train_attack_evalute(N_FAKE_USERS, attack_benign_training_set, test_set, mal_epochs, n_users, n_movies):
+def train_attack_evalute(N_FAKE_USERS, attack_benign_training_set, test_set, mal_epochs, n_users, n_movies, model_path):
     # evaluate the model using the fake predictions
     tf.reset_default_graph()
     model = get_model(n_fake_users=N_FAKE_USERS, n_users=n_users, n_movies=n_movies)
     with tf.Session() as sess:
         model_p = TFPredictWrapper(model, sess)
         saver = tf.train.Saver()
-        saver.restore(sess, "model.ckpt")
+        saver.restore(sess, model_path)
         for e in range(mal_epochs):
             u = attack_benign_training_set[0]
             i = attack_benign_training_set[1]
@@ -297,18 +317,19 @@ def train_attack_evalute(N_FAKE_USERS, attack_benign_training_set, test_set, mal
                                                  model.y_true: r})
 
             mean_hr, mean_ndcg, time_eval = evaluate_model(model_p, test_set, verbose=0)
-            print('e={} loss={:.3f} hr={:.3f} ndcg={:.3f} time[{:.3f}]s'.format(e, epoch_loss, mean_hr, mean_ndcg, time_eval))
+            # print('e={} loss={:.3f} hr={:.3f} ndcg={:.3f} time[{:.3f}]s'.format(e, epoch_loss, mean_hr, mean_ndcg, time_eval))
     return epoch_loss, mean_hr, mean_ndcg
 
 # def train_attack_evalute_batches(N_FAKE_USERS, attack_benign_training_set, test_set, batch_size, mal_epochs, n_users,
 #                                  n_movies):
 #     # evaluate the model using the fake predictions
+#     model_path = f"tf_neumf_models/model{n_fake_users}.ckpt"
 #     tf.reset_default_graph()
 #     model = get_model(n_fake_users=N_FAKE_USERS, n_users=n_users, n_movies=n_movies)
 #
 #     with tf.Session() as sess:
 #         saver = tf.train.Saver()
-#         saver.restore(sess, "model.ckpt")
+#         saver.restore(sess, model_path)
 #         model_p = TFPredictWrapper(model, sess)
 #         for e in range(mal_epochs):
 #             batches = get_batches(attack_benign_training_set, batch_size=batch_size)
@@ -328,7 +349,7 @@ def train_attack_evalute(N_FAKE_USERS, attack_benign_training_set, test_set, mal
 #     return epoch_loss, mean_hr, mean_ndcg
 
 
-def run_expriment(n_fake_users, threshold) -> float:
+def run_expriment(n_fake_users, threshold) -> (float, float):
 
     convert_binary = True
     load_model = False
@@ -336,6 +357,7 @@ def run_expriment(n_fake_users, threshold) -> float:
     num_negatives = 4 # according to paper..
     mal_epochs = 5
     TRAIN_FRAC = 0.1
+    model_path = f"tf_neumf_models/model{n_fake_users}.ckpt"
     print('Started...')
     DATASET_NAME = 'movielens100k'
     # DATASET_NAME = 'movielens1m'
@@ -344,21 +366,39 @@ def run_expriment(n_fake_users, threshold) -> float:
     data = Data(seed=42)
     train_set, test_set, n_users, n_movies = data.pre_processing(df, test_percent=1)
     # TRAIN
-    train_model(n_fake_users, n_users, n_movies, train_set, test_set) # creates a model and saves it
-    predict(n_fake_users, n_users, n_movies, test_set)
+    train_model(n_fake_users, n_users, n_movies, train_set, test_set, model_path) # creates a model and saves it
+    predict(n_fake_users, n_users, n_movies, test_set, model_path)
     #ATTACK
-    attack_user_item_cartesian, attack_users, attack_items = create_attack_input(n_fake_users, n_users, n_movies, train_set)
-    attack_rating_prob = generate_fake_ratings(n_fake_users, n_users, n_movies, train_set, attack_user_item_cartesian)
-    attack_df_filtered = filter_ratings_by_treshold(attack_users, attack_items, attack_rating_prob, threshold)
+    attack_input, attack_users, attack_items, output_dim = create_attack_input(n_fake_users, n_users, n_movies, train_set)
+    attack_rating_prob = generate_fake_ratings(n_fake_users, n_users, n_movies, attack_input, model_path)
+    attack_df_filtered, poison_amount = filter_ratings_by_treshold(attack_users, attack_items, attack_rating_prob, threshold)
     # ATTACK EVALUATION
     mal_training_set = add_negative_samples(attack_df_filtered, num_negatives)
+    # for i in range(5):
     attack_benign_training_set = mix_attack_df_with_training_set(mal_training_set, train_set, TRAIN_FRAC=TRAIN_FRAC)
-    epoch_loss, mean_hr, mean_ndcg = train_attack_evalute(n_fake_users, attack_benign_training_set, test_set, mal_epochs, n_users, n_movies)
+    epoch_loss, mean_hr, mean_ndcg = train_attack_evalute(n_fake_users, attack_benign_training_set, test_set, mal_epochs, n_users, n_movies, model_path)
+    #     print(f'i={i}, mean={mean_hr}')
+    return mean_hr, mean_ndcg, poison_amount
 
-    return mean_hr
 
+import pickle
+import pandas as pd
 
-run_expriment(8, 0.6)
+n_fake_user_list = [2, 4, 8, 16, 32, 64, 128]
+tresholds_list = [0.4, 0.5, 0.6, 0.7, 0.8]
+# n_fake_user_list = [2, 4]
+# tresholds_list = [0.4]
+rows = []
+for i, n_fake_users in enumerate(n_fake_user_list):
+    for j, threshold in enumerate(tresholds_list):
+        mean_hr, mean_ndcg, poison_amount = run_expriment(n_fake_users, threshold)
+        print('Finished exp with: n_fake_users={}, threshold={}, mean_hr= {:0.2f}'.format(n_fake_users, threshold, mean_hr))
+        rows.append([n_fake_users, threshold, mean_hr, mean_ndcg, poison_amount])
+results = pd.DataFrame(np.array(rows), columns =['n_fake_users', 'threshold', 'mean_hr', 'mean_ndcg', 'poison_amount'])
+pd.options.display.max_rows = 1000
+print(results)
+pickle.dump(results, open('out_results.pickle', 'wb'))
+
 
 
 
